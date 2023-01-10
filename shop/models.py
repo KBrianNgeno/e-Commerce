406bfd6719b99da6e73c54c5559a43f57e677a68
Brian Ng'eno <kbrianngeno@gmail.com>
2023-01-10 18:07:09 +0300

    Add simple Product model.

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+    description = models.TextField(blank=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    available = models.BooleanField(default=True)
+    
+    def __str__(self) -> str:
+        return self.name
