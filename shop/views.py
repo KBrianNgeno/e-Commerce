c95f5569918310101b0bf0d5ded7d6020087583b
Brian Ng'eno <kbrianngeno@gmail.com>
2023-01-10 18:28:59 +0300

    Add ProductListView.

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Product
+
+class ProductListView(ListView):
+    model = Product
+    context_object_name = 'products'
+    paginate_by = 10
+    template_name = 'shop/list.html'
